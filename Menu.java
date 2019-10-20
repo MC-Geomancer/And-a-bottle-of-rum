47000dbf27fafeeaab69f274ad09778e13cc24cf
MC-Geomancer <52697843+MC-Geomancer@users.noreply.github.com>
2019-10-20 18:38:15 +0300

    Added one more line of text

--- Menu.java
+++ Menu.java
@@ -2,5 +2,6 @@ public class Menu
 {
     public static void main(String[] args) {
         System.out.println("First git project!");
+        System.out.println("Here is one more line for pull request");
     }
 }
