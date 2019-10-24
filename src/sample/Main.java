f1b57edfa0f9e70cf7b73e1e16b32cd773503d0c
MC-Geomancer <pavlodobro72@gmail.com>
2019-10-24 15:03:51 +0300

    Uploading all javaFX files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("And a bottle of rum!");
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 426, 737));
         primaryStage.show();
     }
